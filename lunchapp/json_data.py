01f11072f29e91007a972d0534c7327dc0bac77f
Narcissist1 <m6106918@gmail.com>
2016-07-01 15:48:59 +0800

    bug fix

--- lunchapp/json_data.py
+++ lunchapp/json_data.py
@@ -22,7 +22,7 @@ def _restaurant_dict(res):
         'address': res.address,
         'spicy_level': res.spicy_level.value if res.spicy_level is not None else '',
         'cuisine': res.cuisine,
-        'images': [QINIU_URL + image for image in res.images]
+        'images': [QINIU_URL + image_name for image_name in res.images.image_url]
     }
     return info
 
