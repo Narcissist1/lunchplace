36bc85a2369b7e57d7a267b467276457e42851b5
Narcissist1 <m6106918@gmail.com>
2016-07-08 19:34:09 +0800

    change logout method

--- lunchapp/views/base.py
+++ lunchapp/views/base.py
@@ -38,8 +38,9 @@ bp.add_url_rule("/login", view_func=Login.as_view("login"))
 
 
 class Logout(MethodView):
-    def put(self):
+    def post(self):
         logout_user()
+        g.user = None
         return 'Logout Success!'
 
 bp.add_url_rule("/logout", view_func=Logout.as_view("logout"))
